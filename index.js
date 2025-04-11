89bb1f1f67451a2a32c1ad4fad9ab25c76ea4601
unknown <abhishekranjansrivastava19@gmail.com>
2025-04-11 12:38:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.get("/generate-barcode", (req, res) => {
       const html = `
 <html>
   <body style="font-family: Arial; display: flex; justify-content: center; align-items: center; padding: 10px;">
-    <div style="border: 1px solid #000; padding: 10px; text-align: center; width: 350px;>
+    <div style="border: 1px solid #000; padding: 10px; text-align: center; width: 350px;">
       <div style="font-size: 20px; font-weight: bold; margin-bottom: 4px;">${cls} (${section}) (${session})</div>
       <div style="font-size: 20px; font-weight: bold; margin-bottom: 4px;">${name.toUpperCase()}</div>
       <div style="font-size: 15px; margin-bottom: 10px;">S/o ${fatherName.toUpperCase()}</div>
