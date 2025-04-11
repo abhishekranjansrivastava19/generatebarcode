fbd364c25688512be89b437c058cd0224560e9ba
unknown <abhishekranjansrivastava19@gmail.com>
2025-04-11 11:36:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ app.get("/generate-barcode", (req, res) => {
     {
       bcid: "code128",
       text: barcodeData,
-      scale: 2,
-      height: 10,
+      scale: 1,
+      height: 7,
       includetext: true,
       textxalign: "center",
       textsize: 15,
