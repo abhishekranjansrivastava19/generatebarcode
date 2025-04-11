2d5e7ecf0a22b83976763f0a6e428c457544d4a4
unknown <abhishekranjansrivastava19@gmail.com>
2025-04-11 11:04:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.get("/generate-barcode", (req, res) => {
       height: 10,
       includetext: true,
       textxalign: "center",
-      textsize: 15,
+      textsize: 20,
     },
     (err, pngBuffer) => {
       if (err) {
@@ -81,9 +81,9 @@ app.get("/generate-barcode", (req, res) => {
 <html>
   <body style="font-family: Arial; display: flex; justify-content: center; align-items: center; padding: 50px;">
     <div style="border: 1px solid #000; padding: 20px; text-align: center; width: 350px;">
-      <div style="font-size: 30px; font-weight: bold; margin-bottom: 4px;">${cls} (${section}) (${session})</div>
-      <div style="font-size: 30px; font-weight: bold; margin-bottom: 4px;">${name.toUpperCase()}</div>
-      <div style="font-size: 30px; margin-bottom: 10px;">S/o ${fatherName.toUpperCase()}</div>
+      <div style="font-size: 20px; font-weight: bold; margin-bottom: 4px;">${cls} (${section}) (${session})</div>
+      <div style="font-size: 25px; font-weight: bold; margin-bottom: 4px;">${name.toUpperCase()}</div>
+      <div style="font-size: 25px; margin-bottom: 10px;">S/o ${fatherName.toUpperCase()}</div>
       <img src="data:image/png;base64,${base64Image}" alt="Barcode" style="width: 100%; height: auto;" />
     </div>
   </body>
